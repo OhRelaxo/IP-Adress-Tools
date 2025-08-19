b749f7273b7d57aefcd1df2402b8eb010515f6ef
Marcel <marcel.ba@proton.me>
2025-08-19 17:19:10 +0200

    added some more helper functions, so that I can now get the IP-Class and if the IP-adress is a public or private adress.

--- Tools/subnet-calc/helper_func.py
+++ Tools/subnet-calc/helper_func.py
@@ -9,3 +9,27 @@ def calculate_octet(host_bits):
                           "end_host": end_host + network, "broadcast": broadcast + network}
         octet_list.append(current_subnet)
     return octet_list
+
+def ip_class(first_octet):
+    if 0 <= first_octet <= 127:
+        return "A"
+    elif 128 <= first_octet <=  191:
+        return "B"
+    elif 192 <= first_octet <= 223:
+        return "C"
+    elif 224 <= first_octet <= 239:
+        return "D"
+    elif 240 <= first_octet <= 255:
+        return "E"
+    else:
+        raise ValueError("the first octet is bigger than 255 :(")
+
+def ip_pub_or_pri(first_octet, second_octet):
+    if first_octet == 10:
+        return "private"
+    elif first_octet == 172 and 16 <= second_octet <= 31:
+        return "private"
+    elif first_octet == 192 and second_octet == 168:
+        return "private"
+    else:
+        return "public"
--- Tools/subnet-calc/subnet-calc.py
+++ Tools/subnet-calc/subnet-calc.py
@@ -1,26 +1,33 @@
-from helper_func import calculate_octet
+from helper_func import calculate_octet, ip_class, ip_pub_or_pri
 
 IP = "192.168.0.1"
 Mask = "28"
 
+# kann ein String sicher als ein Integer convertiert werden?
+
 octet = IP.split(".")
 host_bits = 32 - int(Mask)
-
-# um die Klasse und vlt. den typ (also öffentlich oder privat) alles in Bit umwandeln und dann vergleichen?
+count_host = 2 ** (host_bits - 2)
+count_sub_net = 2 ** int(Mask)
+first_octet = int(octet[0])
+second_octet = int(octet[1])
 
 if host_bits == 0:
     raise ValueError("the subnet mask has to be bigger than 0!")
 if 1 <= host_bits <= 8:
     print(calculate_octet(host_bits))
+    print(ip_class(first_octet))
+    print(ip_pub_or_pri(first_octet, second_octet))
+
     # Nur das Vierte Octet muss berechnet werden!
     pass
-elif 8 > host_bits <= 16:
+elif 8 < host_bits <= 16:
     # Das Dritte und Vierte Octet muss berechnet werden!
     pass
-elif 16 > host_bits <= 24:
+elif 16 < host_bits <= 24:
     # Das Zweite, Dritte und Vierte Octet muss berechnet werden!
     pass
-elif 24 > host_bits <= 31:
+elif 24 < host_bits <= 31:
     # Das Erste, Zweite, Dritte und Vierte Octet muss berechnet werden!
     pass
 else:
