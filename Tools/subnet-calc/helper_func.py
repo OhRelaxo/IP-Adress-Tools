6311fb454bb5ddf1bd8a3839a7591c4c452bc9bc
Marcel <marcel.ba@proton.me>
2025-08-20 09:05:38 +0200

    build everything into functions and made a cool build_subnet function, so I can now build it :)

--- Tools/subnet-calc/helper_func.py
+++ Tools/subnet-calc/helper_func.py
@@ -1,16 +1,14 @@
-def calculate_octet(host_bits):
-    host_range = 2 ** host_bits
-    octet_list = []
-    end_host = host_range - 2
-    broadcast = host_range - 1
-    for i in range(0, int(256 / host_range)): # 256^octet die ich benötige -> /24 -> 2 ...
-        network = i * host_range
-        current_subnet = {"network" : network, "start_host": 1 + network,
-                          "end_host": end_host + network, "broadcast": broadcast + network}
-        octet_list.append(current_subnet)
-    return octet_list
+def calculate_32_bit_ip(ip:str) -> int:
+    ip_list = list(map(int, ip.split(".")))
+    ip_in_bit = 0
+    shift = 24
+    for octet in ip_list:
+        ip_in_bit += octet << shift
+        shift -= 8
+    print(bin(ip_in_bit))
+    return ip_in_bit
 
-def ip_class(first_octet, second_octet):
+def ip_class(first_octet: int, second_octet: int) -> str:
     if 0 <= first_octet <= 126:
         return "A"
     elif first_octet == 127:
@@ -28,7 +26,7 @@ def ip_class(first_octet, second_octet):
     else:
         raise ValueError("the first octet is bigger than 255 :(")
 
-def ip_pub_or_pri(first_octet, second_octet):
+def ip_pub_or_pri(first_octet: int, second_octet: int) -> str:
     if first_octet == 10:
         return "private"
     elif first_octet == 172 and 16 <= second_octet <= 31:
@@ -37,3 +35,38 @@ def ip_pub_or_pri(first_octet, second_octet):
         return "private"
     else:
         return "public"
+
+def fancy_string(bit32_component: int) -> str:
+    return f"{bit32_component >> 24 & 0xFF}.{bit32_component >> 16 & 0xFF}.{bit32_component >> 8 & 0xFF}.{bit32_component >> 0 & 0xFF}"
+
+def build_subnet(ip: str, prefix: int, first_octet: int, second_octet: int):
+    ip_in_bit = calculate_32_bit_ip(ip)
+    subnet_mask = (0xFFFFFFFF << (32 - prefix) & 0xFFFFFFFF)
+    network_adr = ip_in_bit & subnet_mask
+    inverse_mask = subnet_mask ^ 0xFFFFFFFF
+    broadcast = ip_in_bit | inverse_mask
+    ip_cl = ip_class(first_octet, second_octet)
+    ip_type = ip_pub_or_pri(first_octet, second_octet)
+    if prefix > 31:
+        hosts = 0 # kann kein subnetz mit einem Prefix berechnen welcher größer als 31 ist :(
+    else: hosts = 2 ** (32 - prefix) - 2
+    if hosts >= 1:
+        first_host = fancy_string(network_adr + 1)
+        last_host = fancy_string(network_adr + hosts)
+    else:
+        first_host = "-"
+        last_host = "-"
+
+    subnet = {
+        "ip-class": ip_cl,
+        "ip-type": ip_type,
+        "network-address": fancy_string(network_adr),
+        "subnet mask": fancy_string(subnet_mask),
+        "inverse mask": fancy_string(inverse_mask),
+        "broadcast-address": fancy_string(broadcast),
+        "sum subnets": 2 ** prefix,
+        "sum hosts": hosts,
+        "first host": first_host,
+        "last host": last_host
+    }
+    return subnet
--- Tools/subnet-calc/subnet-calc.py
+++ Tools/subnet-calc/subnet-calc.py
@@ -1,4 +1,4 @@
-from helper_func import calculate_octet, ip_class, ip_pub_or_pri
+from helper_func import build_subnet
 
 ip = "192.168.0.1"
 prefix = "28"
@@ -7,61 +7,9 @@ prefix = "28"
 
 def subnet_calculator():
     ip_list = ip.split(".")
-    host_bits = 32 - int(prefix)
-    count_host = 2 ** (host_bits - 2)
-    count_sub_net = 2 ** int(prefix)
     first_octet = int(ip_list[0])
     second_octet = int(ip_list[1])
-
-    if host_bits == 0:
-        raise ValueError("the subnet mask has to be bigger than 0!")
-    if 1 <= host_bits <= 8:
-        print(calculate_octet(host_bits))
-        print(ip_class(first_octet, second_octet))
-        print(ip_pub_or_pri(first_octet, second_octet))
-    # den String in einer eigenen Funktion bauen, vielleicht hier einen "master string" und diesen dann in die Funktion hereingeben!
-    # Nur das Vierte Octet muss berechnet werden!
-    elif 8 < host_bits <= 16:
-    # Das Dritte und Vierte Octet muss berechnet werden!
-        pass
-    elif 16 < host_bits <= 24:
-    # Das Zweite, Dritte und Vierte Octet muss berechnet werden!
-        pass
-    elif 24 < host_bits <= 31:
-    # Das Erste, Zweite, Dritte und Vierte Octet muss berechnet werden!
-        pass
-    else:
-        raise ValueError("host_bits cant be 32!")
-
-    # Das alles noch in Sinnvolle funktionen unterteilen und die Klasse, sowie öffentlich oder privat einbauen.
-
-    ip_list = list(map(int, ip.split(".")))
-    ip_in_bit = 0
-    shift = 24
-    for octet in ip_list:
-        ip_in_bit += octet << shift
-        shift -= 8
-    print(bin(ip_in_bit))
-    mask = (0xFFFFFFFF << (32 - int(prefix)) & 0xFFFFFFFF)
-    network_adr = ip_in_bit & mask
-    inverse_mask = mask ^ 0xFFFFFFFF
-    broadcast = ip_in_bit | inverse_mask
-    print(bin(broadcast))
-    if int(prefix) > 31:
-        return # kann kein subnetz mit einem Prefix berechnen welcher größer als 31 ist :(
-    hosts = 2 ** (32 - int(prefix)) - 2
-    if hosts >= 1:
-        first_host = network_adr + 1
-        last_host = network_adr + hosts
-    else:
-        first_host = "-"
-        last_host = "-"
-    #192.168.0.1
-    print(f"{first_host >> 24 & 0xFF}.{first_host >> 16 & 0xFF}.{first_host >> 8 & 0xFF}.{first_host >> 0 & 0xFF}")
-    # 11000000101010000000000000000001 >> 24 -> 11000000
-    # 11000000101010000000000000000001 >> 16 -> 10101000
-    # 11000000101010000000000000000001 >> 8 -> 0
-    # 11000000101010000000000000000001 >> 0 -> 1
-
+    subnet = build_subnet(ip, int(prefix), first_octet, second_octet)
+    print(subnet)
 
 subnet_calculator()
