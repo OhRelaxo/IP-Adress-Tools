fe2835a8c9b3ad573abe4e2b953240f7b925ffa8
Marcel <marcel.ba@proton.me>
2025-08-20 11:32:15 +0200

    added requirements and tabulate for fancy tables ✨

--- Tools/subnet-calc/subnet-calc.py
+++ Tools/subnet-calc/subnet-calc.py
@@ -1,4 +1,5 @@
 from helper_func import generate_subnets
+from tabulate import tabulate
 
 ip = "192.168.0.0"
 prefix = "28"
@@ -10,8 +11,15 @@ def subnet_calculator():
     first_octet = int(ip_list[0])
     second_octet = int(ip_list[1])
     subnet = generate_subnets(ip, int(prefix), first_octet, second_octet)
-    print(subnet)
+    headers = ["IP-Class", "IP-Type", "Network Address", "Subnet Mask", "Inverse Mask", "Broadcast Address", "Sum Subnets",
+               "Sum Hosts", "First Host", "Last Host"]
 
+    rows = [[entry["ip-class"], entry["ip-type"], entry["network-address"], entry["subnet mask"],
+             entry["inverse mask"], entry["broadcast-address"], entry["sum subnets"],
+             entry["sum hosts"], entry["first host"], entry["last host"]]for entry in subnet]
+
+    table = tabulate(rows, headers=headers, tablefmt='grid')
+    print(table)
 
 subnet_calculator()
 
